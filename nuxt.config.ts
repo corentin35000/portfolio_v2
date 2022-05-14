37458d143abb007d2fa13cc56957dd72efa9cec5
Coco <corentin.recanzone@epitech.eu>
2022-05-14 11:56:00 +0200

    Add directory and initialisation : store create and init directory for index.js add, public in to robots.txt and favicon.ico, add SCSS/SASS, install module sitemap for nuxtjs, typescript in strict true, ssr project, builder in vite, init one page index.vue in 'pages'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    typescript: {
+        strict: true
+    },
+    builder: 'vite',
+    ssr: true,
+    modules: [
+        '@nuxtjs/sitemap' // ! Déclarez toujours le module sitemap à la fin du tableau !
+    ],
+    sitemap: {
+        // options
+    },
 })
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+   INDEX
+
+  <div id="ttt">
+    dfdf
+
+    <h2></h2>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  useHead({
+    title: 'My App',
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'UTF-8',
+    meta: [
+      { name: 'description', content: 'My amazing site.' }
+    ],
+  })
+</script>
+
+<style lang="scss" scoped>
+  #ttt {
+    color: red;
+
+    h2 {
+      color: green;
+    }
+  }
+</style>
